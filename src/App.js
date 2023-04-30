f3b5324bbd4a1b6d61c5d8481e42c5106d9b025b
Nidhi Tandon <nidhitandon08@gmail.com>
2023-04-30 11:13:14 +0530

    feat: initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MenuComponent from "./MenuComponent/MenuComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <MenuComponent/>
+    </div>
+  );
+}
+
+export default App;
